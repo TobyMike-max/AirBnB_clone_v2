5466beebf3dcd5ba2c8d77653dac782600978aad
TobyMike-max <osinubi.michael@gmail.com>
2022-10-15 08:46:34 +0100

    [Modified]: Route string

--- web_flask/3-python_route.py
+++ web_flask/3-python_route.py
@@ -24,7 +24,7 @@ def c_text(text):
     return ("C {}".format(text))
 
 
-@app.route('/python/<text>', strict_slashes=False)
+@app.route('/python/(<text>)', strict_slashes=False)
 def python_str(text='is cool'):
     """Return Python followed by value of text"""
     text = text.replace('_', ' ')
