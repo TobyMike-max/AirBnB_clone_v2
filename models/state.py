7909f2d612c0b5b15e4fa65dadbff9986a4af803
TobyMike-max <osinubi.michael@gmail.com>
2022-10-16 17:26:01 +0100

    [Modified]

--- models/state.py
+++ models/state.py
@@ -11,7 +11,7 @@ class State(BaseModel, Base):
     if getenv("HBNB_TYPE_STORAGE") == "db":
         __tablename__ = "states"
         name = Column(String(128), nullable=False)
-        cities = relationship("City", backref='state',
+        cities = relationship("City", back_populates='state',
                 cascade='all, delete, delete-orphan')
     else:
         name = ""
