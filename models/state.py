f811ebc946ea54439412b91544092503b8174f66
TobyMike-max <osinubi.michael@gmail.com>
2022-10-16 16:17:01 +0100

    [Modified]

--- models/state.py
+++ models/state.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 """ State Module for HBNB project """
-import models
+from os import getenv
 from models.base_model import BaseModel, Base
 from sqlalchemy import Column, String
 from sqlalchemy.orm import relationship
@@ -10,18 +10,14 @@ from sqlalchemy.orm import relationship
 
 class State(BaseModel, Base):
     """ State class """
-    if models.storage_t == "db":
+    if getenv("HBNB_TYPE_STORAGE") == "db":
         __tablename__ = "states"
         name = Column(String(128), nullable=False)
         cities = relationship("City", backref='state')
     else:
         name = ""
 
-    def __init__(self, *args, **kwargs):
-        """Initializes state"""
-        super().__init__(*args, **kwargs)
-
-    if models.storage_t != "db":
+    if getenv("HBNB_TYPE_STORAGE") != "db":
         @property
         def cities(self):
             """Getter for list of City instances related to State"""
