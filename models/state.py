2fc73a9d5ecd73cd6745bbde0790cd8724be028b
TobyMike-max <osinubi.michael@gmail.com>
2022-10-17 10:21:19 +0100

    [Modified]: Fixed PEP8 validation errors

--- models/state.py
+++ models/state.py
@@ -12,10 +12,10 @@ class State(BaseModel, Base):
         __tablename__ = "states"
         name = Column(String(128), nullable=False)
         cities = relationship("City", back_populates='state',
-                cascade='all, delete, delete-orphan')
+                              cascade='all, delete, delete-orphan')
     else:
         name = ""
-        
+
         @property
         def cities(self):
             """Getter for list of City instances related to State"""
