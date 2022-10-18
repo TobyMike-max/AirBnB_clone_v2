7f93bbbde0df0f0266a89f589daf36d9af5e7f29
TobyMike-max <osinubi.michael@gmail.com>
2022-10-18 14:19:06 +0100

    [Modified]: Changed "all" API argument to State class and added kwarg Exception for app.teardown

--- web_flask/7-states_list.py
+++ web_flask/7-states_list.py
@@ -2,17 +2,18 @@
 """ Script that starts a Flask web application """
 from flask import Flask, render_template
 from models import storage
+from models.state import State
 app = Flask(__name__)
 
 
 @app.route('/states_list', strict_slashes=False)
 def list_states():
     """Display a HTML page with the states listed in alphabetical order."""
-    states = sorted(list(storage.all(cls='State').values()), key=lambda x: x.name)
+    states = sorted(list(storage.all(State).values()), key=lambda x: x.name)
     return render_template('7-states_list.html', states=states)
 
 @app.teardown_appcontext
-def session_close():
+def session_close(Exception=None):
     """Closes the storage on teardown"""
     storage.close()
 
