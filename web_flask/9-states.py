386517517286fdded27629a592cb6065f49d18df
TobyMike-max <osinubi.michael@gmail.com>
2022-10-19 03:48:30 +0100

    [Modified]: Fixed missing return keyword

--- web_flask/9-states.py
+++ web_flask/9-states.py
@@ -8,7 +8,7 @@ app = Flask(__name__)
 
 @app.route('/states', strict_slashes=False)
 @app.route('/states/<state_id>', strict_slashes=False)
-def states(state_id):
+def states(state_id=None):
     """Display a HTML page with a list of all State object"""
     states = storage.all(State)
     if state_id:
